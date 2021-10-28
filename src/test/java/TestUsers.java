b513be2f8e247431e553c0752877e6ce786e8d1e
ShubhamB2595 <shubhambhele95@gmail.com>
2021-10-28 10:28:27 +0530

    UC10 - JUnit Test to validate the User

---
+++ src/test/java/TestUsers.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestUsers {
+	
+	UserValidation testing = new UserValidation();
+
+	@Test
+	public void testingUsers() {
+		
+		testing.validFirstName("Shubham");
+		testing.validLastName("Bhele");
+		testing.validEmail("shubhambhele95@gmail.com");
+		testing.validMobile("91 8698348143");
+		testing.validPassword("Password#1");
+		
+	}
+
+}
