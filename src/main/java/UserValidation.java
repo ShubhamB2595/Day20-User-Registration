4c908ece66ebf127d42acbd95ef45748de79270a
ShubhamB2595 <shubhambhele95@gmail.com>
2021-10-28 19:11:50 +0530

    UC12 - refactored the code to throw custom exceptions

--- src/main/java/UserValidation.java
+++ src/main/java/UserValidation.java
@@ -9,63 +9,95 @@ public class UserValidation {
 	}
 
 	//method for first name
-	public void validFirstName(String firstName) {
+	public String validFirstName(String firstName) {
 		
-		String regexFN = "^[A-Z]{1}[a-zA-Z]{2,}";
-        Pattern p = Pattern.compile(regexFN);
-        Matcher matcher = p.matcher(firstName);
-        boolean result = matcher.matches();
-        if(result) {
-        	System.out.println("Valid First Name: " + firstName);
+        try {
+        	String regexFN = "^[A-Z]{1}[a-zA-Z]{2,}";
+            Pattern p = Pattern.compile(regexFN);
+            Matcher matcher = p.matcher(firstName);
+            boolean result = matcher.matches();
+        	if(result) {
+        		System.out.println("Valid First Name: " + firstName);
+            	return "Valid";
+            }
+        	else {
+        		System.out.println("Invalid First Name: " + firstName);
+            	return "Invalid";
+        	}
         }
-        else {
-        	System.out.println("Invalid First Name: " + firstName);
+        catch(Exception e) {
+        	System.out.println("Something went wrong");
         }
+		return firstName; 
 	}
 	
 	//method for last name
-		public void validLastName(String lastName) {
+		public String validLastName(String lastName) {
 			
-			String regexLN = "^[A-Z]{1}[a-zA-Z]{2,}";
-	        Pattern p = Pattern.compile(regexLN);
-	        Matcher matcher = p.matcher(lastName);
-	        boolean result = matcher.matches();
-	        if(result) {
-	        	System.out.println("Valid Last Name: " + lastName);
-	        }
-	        else {
-	        	System.out.println("Invalid Last Name: " + lastName);
-	        }
+			try {
+				String regexLN = "^[A-Z]{1}[a-zA-Z]{2,}";
+		        Pattern p = Pattern.compile(regexLN);
+		        Matcher matcher = p.matcher(lastName);
+		        boolean result = matcher.matches();
+		        if(result) {
+		        	System.out.println("Valid Last Name: " + lastName);
+		        	return "Valid";
+		        }
+		        else {
+		        	System.out.println("Invalid Last Name: " + lastName);
+		        	return "Invalid";
+		        }
+			}
+			catch(Exception e) {
+				System.out.println("Something went wrong");
+			}
+			return lastName;
 		}
 		
 		//method for Email ID 
-		public void validEmail(String email) {
-					
-			String regexemail = "^[0-9a-zA-Z]+([_+-.][0-9a-zA-Z]+)*+" + "@[0-9a-zA-Z]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2})?$";
-			Pattern p = Pattern.compile(regexemail);
-			Matcher matcher = p.matcher(email);
-			boolean result = matcher.matches();
-			if(result) {
-				System.out.println("Valid Email ID: " + email);
+		public String validEmail(String email) {
+			
+			try {
+				String regexemail = "^[0-9a-zA-Z]+([_+-.][0-9a-zA-Z]+)*+" + "@[0-9a-zA-Z]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2})?$";
+				Pattern p = Pattern.compile(regexemail);
+				Matcher matcher = p.matcher(email);
+				boolean result = matcher.matches();
+				if(result) {
+					System.out.println("Valid Email ID: " + email);
+					return "Valid";
+				}
+		        else {
+		        	System.out.println("Invalid Email ID: " + email);
+		        	return "Invalid";
+		        }
 			}
-	        else {
-	        	System.out.println("Invalid Email ID: " + email);
-	        }
+			catch(Exception e) {
+				System.out.println("Something went wrong");
+			}
+			return email;
 		}
 		
 		//method for mobile number as Country code follow by space and 10 digit number
-		public void validMobile(String mobile) {
-							
-			String regexmobile = "^[0-9]{1,2}[' '][0-9]{10}$";
-			Pattern p = Pattern.compile(regexmobile);
-			Matcher matcher = p.matcher(mobile);
-			boolean result = matcher.matches();
-			if(result) {
-				System.out.println("Valid Mobile Number: " + mobile);
+		public String validMobile(String mobile) {
+			
+			try {
+				String regexmobile = "^[0-9]{1,2}[' '][0-9]{10}$";
+				Pattern p = Pattern.compile(regexmobile);
+				Matcher matcher = p.matcher(mobile);
+				boolean result = matcher.matches();
+				if(result) {
+					System.out.println("Valid Mobile Number: " + mobile);
+					return "Valid";
+				}
+		        else {
+		        	System.out.println("Invalid Mobile Number: " + mobile);
+		        	return "Invalid";
+		        }
 			}
-	        else {
-	        	System.out.println("Invalid Mobile Number: " + mobile);
-	        }
+			catch(Exception e) {
+				System.out.println("Something went wrong");
+			}
+			return mobile;
 		}
 		
 		//method for password as Rules 
@@ -75,17 +107,25 @@ public class UserValidation {
 		 * Rule3 - at least 1 numeric number
 		 * Rule4 - should have 1 special character
 		 */
-		public void validPassword(String password) {
-									
-			String regexPass = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8,}";
-			Pattern p = Pattern.compile(regexPass);
-			Matcher matcher = p.matcher(password);
-			boolean result = matcher.matches();
-			if(result) {
-				System.out.println("Valid Password: " + password);
+		public String validPassword(String password) {
+			
+			try {
+				String regexPass = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8,}";
+				Pattern p = Pattern.compile(regexPass);
+				Matcher matcher = p.matcher(password);
+				boolean result = matcher.matches();
+				if(result) {
+					System.out.println("Valid Password: " + password);
+					return "Valid";
+				}
+		        else {
+			       	System.out.println("Invalid Password: " + password);
+			       	return "Invalid";
+		       }
 			}
-	        else {
-		       	System.out.println("Invalid Password: " + password);
-	       }
+			catch(Exception e) {
+				System.out.println("Something went wrong");
+			}
+			return password;
 		}
 }
--- src/test/java/TestUsers.java
+++ src/test/java/TestUsers.java
@@ -1,19 +1,33 @@
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-
+/*
+ * class for testing the users data
+ */
 public class TestUsers {
+	
+	//declaration
+	String result;
 
 	UserValidation testing = new UserValidation();
 
 	@Test
 	public void testingUsers() {
 		
-		testing.validFirstName("Shubham");
-		testing.validLastName("Bhele");
+		result = testing.validFirstName("Shubham");
+		assertEquals(result, "Valid");
+		
+		result = testing.validLastName("Bhele");
+		assertEquals(result, "Valid");
+		
 		testing.validEmail("shubhambhele95@gmail.com");
+		assertEquals(result, "Valid");
+		
 		testing.validMobile("91 8698348143");
+		assertEquals(result, "Valid");
+		
 		testing.validPassword("Password#1");
+		assertEquals(result, "Valid");
 		
 	}
 
